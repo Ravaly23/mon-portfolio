d7d0070f90e06b1a9d30310d71096742e7b1378b
Ravaly23 <valeryravaly2003@gmail.com>
2026-04-15 21:25:25 +0300

    essai

--- src/Pages/MonPorteFolio.tsx
+++ src/Pages/MonPorteFolio.tsx
@@ -6,25 +6,25 @@ export default function MonPorteFolio() {
   // const [ turned , setTurned ] = useState(false);
   return (
     <div className="min-h-screen bg-[#0a0a0f] text-[#e8e8ec]">
-      <header className="dark:bg-gray-950 backdrop-blur-md pt-4 pb-4 pl-10 pr-20">
-        <nav className="flex items-center justify-between">
+      <header className="dark:bg-gray-950 backdrop-blur-md pt-4 pb-4 pl-7 pr-7">
+        <nav className="flex items-center justify-between border border-white ">
           <div className="rounded-4xl border border-white pt-1.5 pb-1 w-12 md:w-12 block ">
             <h1 className="text-3xl font-serif text-center inline-block hover:rotate-90 duration-500 ease-linear hover:cursor-pointer">
               <span className="text-orange-400">V</span>
               <span className="text-white">R</span>
             </h1>
           </div>
-          <ul className="hidden md:flex pt-2 justify-between  text-xl md:gap-x-4">
+          <ul className="hidden md:flex pt-2 justify-between ml-10 text-xl md:gap-x-4">
             <LienNavigation />
           </ul>
           <div className="md:hidden">
             <Hamburger toggled={isOpen} toggle={setOpen} />
             {isOpen && (
               <>
-                <nav className="flex flex-col fixed bg-[#0a0a0f] text-2xl w-30 items-center">
-                  <div className={`border-blue-900 bg-[#0a0a0f] w-6 h-6 rotate-45 mr-18 -mt-1`}></div>
+                <nav className="flex flex-col fixed bg-[#0a0a0f] text-2xl w-30 items-center right-2">
+                  <div className={` bg-[#0a0a0f] w-6 h-6 rotate-45 -mt-1 ml-8`}></div>
                   <LienNavigation />
-                </nav>
+                </nav> 
               </>
             )}
           </div>
